da91aca3935aea259200d2ab84b7d028079ff394
Chien Pham <chienphamvu@gmail.com>
2019-05-24 17:30:56 +0700

    Add security group in terraform

--- master.tf
+++ master.tf
@@ -27,16 +27,16 @@ provider "aws" {
 #          Public subnet
 ##################################
 
-# resource "aws_subnet" "myvpc-public-subnet" {
-#   vpc_id = "${aws_vpc.myvpc.id}"
-#   cidr_block = "10.0.1.0/24"
-#   map_public_ip_on_launch = "true"
-#   availability_zone = "us-east-2a"
+resource "aws_subnet" "myVPC-public-subnet" {
+  vpc_id = "${var.aws_vpc_id}"
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-2a"
 
-#   tags {
-#     Name = "myvpc-public"
-#   }
-# }
+  tags {
+    Name = "myvpc-public"
+  }
+}
 
 # resource "aws_route_table" "route-table-public" {
 #   vpc_id = "${aws_vpc.myvpc.id}"
@@ -102,6 +102,25 @@ provider "aws" {
 #          Instance
 ##################################
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH traffic"
+  vpc_id      = "${var.aws_vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "admin" {
+  key_name = "private-key"
+
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDd98QYvKr7toHasiMJA1TOSCiQ75YcncMLnYkG+FTgRPcmSHZAq8499X7PAvi1CPiYYlNDY8qKpn8AqojDmaTU6jJU8QTolE6HsCDzZoohp781KETsb+EBtp2Ba1UHPAXPD1Uotg8lhfCyk3fgwYZO4hhOS82MXX/AoXV/frGFciI9WYEG9WjN01GhX8yJvRoLCxyke6es7eHV8bWUlGU68cOH+xyGUkt++rNmnxn0YZeUbDTEQChYdnH91OovRdHdXf+pXW2ZHwKiy1h0fSKc8vFzSQmiCOVHhBlZAX+xlRntUWTrCEvNtkMBeF+Wr6Td83EFAjkH8h2HMThQtDib pvchien@pvchien-comp"
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -118,24 +137,24 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# resource "aws_network_interface" "private_network_interface" {
-#   subnet_id   = "${var.aws_private_subnet_id}"
-
-#   tags = {
-#     Name = "private_network_interface"
-#   }
-# }
-
 resource "aws_instance" "master" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["${var.aws_private_security_group_id}"]
-  subnet_id = "${var.aws_private_subnet_id}"
+  ami                     = "${data.aws_ami.ubuntu.id}"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = ["${aws_security_group.allow_ssh.id}"]
+  subnet_id               = "${var.aws_private_subnet_id}"
+  key_name                = "${aws_key_pair.admin.id}"
 
-  # network_interface {
-  #   network_interface_id = "${aws_network_interface.private_network_interface.id}"
-  #   device_index = 0
-  # }
+  tags = {
+    Name = "Master node"
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami                     = "${data.aws_ami.ubuntu.id}"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = ["${aws_security_group.allow_ssh.id}"]
+  subnet_id               = "${aws_subnet.myVPC-public-subnet.id}"
+  key_name                = "${aws_key_pair.admin.id}"
 
   tags = {
     Name = "Master node"
--- variables.tf
+++ variables.tf
@@ -19,7 +19,3 @@ variable "aws_vpc_id" {
 variable "aws_private_subnet_id" {
   description = "AWS private subnet ID"
 }
-
-variable "aws_private_security_group_id" {
-  description = "AWS private security group ID"
-}
