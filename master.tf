4b8ebf4c3f3968c611dfe77c61ac69fd47c6890e
Chien Pham <chienphamvu@gmail.com>
2019-06-10 00:29:51 +0700

    Add Container Linux Configuration

--- master.tf
+++ master.tf
@@ -1,31 +1,10 @@
 provider "aws" {
-  region     = "us-east-2"
+  region     = "ap-northeast-1"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
 
   version = "~> 2.0"
 }
-
-# resource "aws_vpc" "myvpc" {
-#   cidr_block = "10.0.0.0/16"
-#   instance_tenancy = "default"
-
-#   tags {
-#     Name = "myvpc"
-#   }
-# }
-
-# resource "aws_internet_gateway" "mygateway" {
-#   vpc_id = "${aws_vpc.myvpc.id}"
-
-#   tags {
-#     Name  = "main"
-#   }
-# }
-
-##################################
-#          Public subnet
-##################################
 
 resource "aws_subnet" "myVPC-public-subnet" {
   vpc_id = "${var.aws_vpc_id}"
@@ -37,70 +16,6 @@ resource "aws_subnet" "myVPC-public-subnet" {
     Name = "myvpc-public"
   }
 }
-
-# resource "aws_route_table" "route-table-public" {
-#   vpc_id = "${aws_vpc.myvpc.id}"
-
-#   route {
-#     cidr_block = "10.0.1.0/24"
-#     gateway_id = "${aws_internet_gateway.mygateway.id}"
-#   }
-
-#   tags {
-#     Name = "my-route-table"
-#   }
-# }
-
-# resource "aws_route_table_association" "route-table-public-subnet" {
-#   subnet_id = "${aws_subnet.myvpc-public-subnet.id}"
-#   route_table_id = "${aws_route_table.route-table.id}"
-# }
-
-##################################
-#          Private subnet
-##################################
-
-# resource "aws_subnet" "myvpc-private-subnet" {
-#   vpc_id = "${aws_vpc.myvpc.id}"
-#   cidr_block = "10.0.3.0/24"
-#   map_public_ip_on_launch = "false"
-#   availability_zone = "us-east-2a"
-
-#   tags {
-#     Name = "myvpc-private"
-#   }
-# }
-
-# resource "aws_eip" "my-eip" {
-#   vpc = true
-# }
-
-# resource "aws_nat_gateway" "my-nat-gateway" {
-#   allocation_id = "${aws_eip.my-eip.id}"
-#   subnet_id = "${aws_subnet.myvpc-public-subnet.id}"
-#   depends_on = ["aws_internet_gateway.mygateway"]
-# }
-
-# resource "aws_route_table" "route-table-private" {
-#   vpc_id = "{aws_vpc.myvpc.id}"
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = "${aws_nat_gateway.my-nat-gateway.id}"
-#   }
-
-#   tags {
-#     Name = "aws-route-table"
-#   }
-# }
-
-# resource "aws_route_table_association" "route-table-private-subnet" {
-#   subnet_id = "${aws_subnet.myvpc-private-subnet.id}"
-#   route_table_id = "${aws_route_table.route-table-private.id}"
-# }
-
-##################################
-#          Instance
-##################################
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
@@ -149,14 +64,17 @@ resource "aws_instance" "master" {
   }
 }
 
-resource "aws_instance" "bastion" {
-  ami                     = "${data.aws_ami.ubuntu.id}"
-  instance_type           = "t2.micro"
-  vpc_security_group_ids  = ["${aws_security_group.allow_ssh.id}"]
-  subnet_id               = "${aws_subnet.myVPC-public-subnet.id}"
-  key_name                = "${aws_key_pair.admin.id}"
+resource "aws_ebs_volume" "data" {
+  availability_zone = "us-west-2a"
+  size              = 3
 
   tags = {
-    Name = "Master node"
+    Name = "HelloWorld"
   }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.data.id}"
+  instance_id = "${aws_instance.master.id}"
 }
