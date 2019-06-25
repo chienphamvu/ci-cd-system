7fc0c9c833fce5b8fc3304c401ea89c385dab56f
Khai Phan <pdkhai@tma.com.vn>
2019-06-25 21:51:36 +0700

    Add docker to jenkins-slave

--- jenkins-jobs/deploy.sh
+++ jenkins-jobs/deploy.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 GCLOUD_SDK_VERSION="251.0.0"
-GCLOUD_FORCE_UPDATE="false"
-
-GCLOUD_PROJECT_NAME="ci-system"
-GCLOUD_COMPUTE_ZONE="asia-east1-a"
+GCLOUD_FORCE_UPDATE=${GCLOUD_FORCE_UPDATE:-false}
+GCLOUD_PROJECT_ID=${GCLOUD_PROJECT_ID:-web-app-244813}
+GCLOUD_COMPUTE_ZONE=${GCLOUD_COMPUTE_ZONE:-asia-east1-a}
+GCLOUD_K8S_CLUSTER_NAME=${GCLOUD_K8S_CLUSTER_NAME:-web-app-cluster}
 
 set -xe
+
+GIT_SHA=$(git rev-parse HEAD)
+
+# Build docker image
+DOCKER_IMAGE="chienphamvu/php-apache"
+
+docker build -t ${DOCKER_IMAGE}:latest -t ${DOCKER_IMAGE}:${GIT_SHA} .
+
+docker login --username $DOCKER_USR --password $DOCKER_PWD
+docker push ${DOCKER_IMAGE}:latest
+docker push ${DOCKER_IMAGE}:${GIT_SHA}
 
 # Install Google Cloud SDK
 if [ ! -d "$HOME/google-cloud-sdk" ] || [[ $GCLOUD_FORCE_UPDATE == "true" ]]; then
@@ -19,8 +30,10 @@ gcloud components update kubectl
 
 # Authenticate with Google Cloud
 gcloud auth activate-service-account --key-file "$K8S_SERVICE_ACCOUNT_FILE"
-gcloud config set project $GCLOUD_PROJECT_NAME
+gcloud config set project $GCLOUD_PROJECT_ID
 gcloud config set compute/zone $GCLOUD_COMPUTE_ZONE
+gcloud container clusters get-credentials $GCLOUD_K8S_CLUSTER_NAME
 
-pwd
-ls
+# Deploy to K8S
+kubectl apply -f k8s
+kubectl set image deployments/php-apache server=${DOCKER_IMAGE}:${GIT_SHA}
